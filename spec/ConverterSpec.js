ea2681f2960014f90f8ea0eac868063dc9c28beb
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-04-10 20:01:56 +0100

    Add Gallons to Litres tests, pass

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -3,7 +3,6 @@ describe("Cooking Ingredients Converter", function() {
   describe("Temperatures", function() {
     it("Fahrenheit to Celsius", function() {
       var result = convertFahrenheitToCelsius(100);
-
       expect(result).toEqual('37.8');
 
       result = convertFahrenheitToCelsius(90);
@@ -16,4 +15,34 @@ describe("Cooking Ingredients Converter", function() {
       expect(result).toEqual('95.0');
     })
   });
+
+  describe("Weight", function () {
+    it("Pounds to Kilos", function() {
+      var result = convertPoundsToKilos(100);
+      expect(result).toEqual('45.4')
+
+      result = convertPoundsToKilos(135);
+      expect(result).toEqual('61.2')
+    })
+
+    it("Kilos to Pounds", function() {
+      var result = convertKilosToPounds(243);
+
+      expect(result).toEqual('535.7')
+    })
+  })
+
+  describe("LiquidVolume", function () {
+    it("Litre to Gallons", function () {
+      var result = convertLiterToGallons(73.6)
+
+      expect(result).toEqual('16.2')
+    })
+
+    it("Gallons to Litres", function () {
+      var result = convertGallonsToLitres(34)
+
+      expect(result).toEqual('154.6')
+    })
+  })
 });
--- src/Converter.js
+++ src/Converter.js
@@ -9,3 +9,27 @@ function convertCelsiusToFahrenheit(temperature) {
   var fahrenheit = (temperature * 1.8 + 32).toFixed(1)
   return fahrenheit
 }
+
+function convertPoundsToKilos(weight) {
+  var kilos = (weight * 0.4536).toFixed(1)
+
+  return kilos
+}
+
+function convertKilosToPounds(weight) {
+  var pounds = (weight * 2.20462262).toFixed(1)
+
+  return pounds
+}
+
+function convertLiterToGallons(liquid) {
+  var gallons = (liquid * 0.22).toFixed(1)
+
+  return gallons
+}
+
+function convertGallonsToLitres(liquid) {
+  var litres = (liquid * 4.54609188).toFixed(1)
+
+  return litres
+}
