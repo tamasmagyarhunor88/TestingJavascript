13cae576e9d757881827214cfdd3be74c80c223c
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-04-10 19:46:12 +0100

    Add Celsius to Fahrenheit test, pass

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -5,6 +5,15 @@ describe("Cooking Ingredients Converter", function() {
       var result = convertFahrenheitToCelsius(100);
 
       expect(result).toEqual('37.8');
+
+      result = convertFahrenheitToCelsius(90);
+      expect(result).toEqual('32.2');
     });
+
+    it("Celsius to Fahrenheit", function() {
+      var result = convertCelsiusToFahrenheit(35);
+
+      expect(result).toEqual('95.0');
+    })
   });
 });
--- src/Converter.js
+++ src/Converter.js
@@ -4,3 +4,8 @@ function convertFahrenheitToCelsius(temperature) {
   var celsius = ((temperature - 32)/1.8).toFixed(1)
   return celsius
 }
+
+function convertCelsiusToFahrenheit(temperature) {
+  var fahrenheit = (temperature * 1.8 + 32).toFixed(1)
+  return fahrenheit
+}
