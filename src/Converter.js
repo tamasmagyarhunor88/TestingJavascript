e6c53a29348d432e0e6d2b6154107d7f10405723
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-04-10 22:02:25 +0100

    Html implementation converter

--- src/Converter.js
+++ src/Converter.js
@@ -1,8 +1,8 @@
 /** Unit Converter **/
 function convertFahrenheitToCelsius(temperature) {
   //conversion code
-  var celsius = ((temperature - 32)/1.8).toFixed(1)
-  return celsius
+  var celsius = ((temperature - 32)/1.8)
+  return celsius.toFixed(1)
 }
 
 function convertCelsiusToFahrenheit(temperature) {
